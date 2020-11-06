2e8e5f7d5e61c1df5916b579dfacfa1ceaf0b57b
fixprogram <fixprogram@mail.ru>
2020-11-06 13:28:47 +0300

    Were added routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+      <Route>This is 404 page</Route>
+    </Switch>
+  );
 };
 
 export default App;
